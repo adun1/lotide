461d8baf3fa75cae41b66196563f60696a28e4a8
adun1 <bmccormack15@gmail.com>
2020-05-26 03:20:45 +0000

    tailCompleted

---
+++ tail.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (!checkIfArrayEqual(actual, expected))
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  else
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+const checkIfArrayEqual = function(a, b) {
+  if (a.length !== b.length)
+    return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i])
+      return false;
+  }
+  return true;
+};
+
+//assuming an array is passed into the function
+//  no checks are in place to protect against that
+const tail = function(object1) {
+  return object1.slice(1);
+};
+
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+assertEqual(tail([]), []);
+assertEqual(tail([1]),[]);
+assertEqual(tail([1,2]), [2]);
+
+const words = [1,2,3];
+tail(words);
+assertEqual(words.length, 3);
