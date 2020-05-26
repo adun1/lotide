640f6d04acb0e20567bad5a3ef07a8983244b3e9
adun1 <bmccormack15@gmail.com>
2020-05-26 23:28:44 +0000

    assert ArraysEqual and AssertArrayEqual exercises

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  else
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length)
+    return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i])
+      return false;
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
+
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+
+console.log(assertEqual(eqArrays([], []), true));
+console.log(assertEqual(eqArrays(0, []), false));
+
+console.log(assertEqual(eqArrays([1],[]), false));
+console.log(assertEqual(eqArrays([],[1]), false));
+console.log(assertEqual(eqArrays([1,2], [2]), false));
