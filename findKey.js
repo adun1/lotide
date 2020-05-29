b65a3af2078b2a847a50a03f294e12629bb07783
adun1 <bmccormack15@gmail.com>
2020-05-29 04:16:51 +0000

    completed findKey

---
+++ findKey.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  else
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+//assuming we are not returning an array
+//  only returning the first key found
+const findKey = function(mainObject, callback) {
+  for (const item of Object.keys(mainObject)) {
+    if (callback(mainObject[item])) {
+      return item;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "test": {stars: '2'},
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 2), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 3), undefined);
