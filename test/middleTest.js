b736e577df7f5964bb42ec0af4a30a42160b775f
adun1 <bmccormack15@gmail.com>
2020-06-02 04:18:49 +0000

    refactored middleTest to use mocha and chai testing libraries

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,37 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-//base case length <= 2
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
+describe("#middle", () => {
+  //case where length <= 2
+  it('returns [] for []', () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-//odd array length
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it('returns [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-//even array length
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it('returns [] for [1,2]', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  //odd array length
+  it('returns [2] for [1,2,3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('returns [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  //even array length
+  it('returns [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('returns [3,4] for [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
